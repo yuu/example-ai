3b547a73b124faa8a18268f07f3c7755282dd580
yuu <yuu@users.noreply.github.com>
2024-10-26 17:52:32 +0900

    chore: install cloudscape

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: [
+    "@cloudscape-design/components",
+    "@cloudscape-design/component-toolkit",
+    "@cloudscape-design/code-view",
+    "@cloudscape-design/chat-components",
+  ],
 };
 
 export default nextConfig;
